8ca63fb792edcb7f3538eac920a47ca8a5791772
Abdur Razzak <abdur.razzak@silverlakeaxis.com>
2024-11-04 17:45:14 +0800

    docker uses in microservice architectures is in progress

---
+++ product/src/test/java/com/mir00r/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mir00r.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
